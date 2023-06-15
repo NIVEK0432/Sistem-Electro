7c0b310bff82f2aa49a42fc939b2c398c8cc4f54
NIVEK0432 <132258474+NIVEK0432@users.noreply.github.com>
2023-06-15 02:00:22 -0600

    Update FuerzaMagnetica.m

--- FuerzaMagnetica.m
+++ FuerzaMagnetica.m
@@ -1,20 +1,21 @@
-% Grafica de la magnitud de la fuerza magnetica
+% Grafica de la magnitud de la fuerza magnética
 % Este código calcula y grafica la magnitud de la fuerza magnética (F) ejercida por un dipolo magnético (mu) de una góndola circular de radio (R), en función de la posición vertical (z), cuando una corriente (I) circula por la góndola.
+
 % Parámetros
-mu = 6.25;          % Dipolo magnético de la góndola 
-mu0 = 4*pi*10^(-7);  % Permeabilidad magnética del vacío 
-I = 1;               % Corriente 
-R = 2.5;             % Radio de la góndola 
+mu = 6.25;          % Dipolo magnético de la góndola (unidades dependen del contexto, como A*m^2)
+mu0 = 4*pi*10^(-7);  % Permeabilidad magnética del vacío (Tesla metro por amperio, T*m/A)
+I = 1;               % Corriente (1 A)
+R = 2.5;             % Radio de la góndola (2.5 m)
 
 % Valores de z
-z = linspace(-10, 10, 100);  % Rango de valores para z
+z = linspace(-10, 10, 100);  % Rango de valores para z, generando 100 puntos equidistantes en el intervalo -10 a 10 metros
 
 % Cálculo de la fuerza magnética F
 f = (3/2) * I * mu * mu0 * R^2 .* z ./ (R^2 + z.^2).^(5/2);
 
 % Gráfica
 plot(z, f, 'LineWidth', 2);
-xlabel('z (m)');
-ylabel('(N)');
-title('Magnitud de la Fuerza Magnética vs Z');
-grid on;
+xlabel('z (m)');  % Etiqueta del eje x
+ylabel('(N)');    % Etiqueta del eje y
+title('Magnitud de la Fuerza Magnética vs Z');  % Título de la gráfica
+grid on;  % Activar cuadrícula en la gráfica
