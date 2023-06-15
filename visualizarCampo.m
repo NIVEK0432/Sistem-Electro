da207a7807355c4454b67dbdf19cac4b61f404d6
NIVEK0432 <132258474+NIVEK0432@users.noreply.github.com>
2023-06-15 01:26:03 -0600

    Update visualizarCampo.m

--- visualizarCampo.m
+++ visualizarCampo.m
@@ -1,3 +1,4 @@
+% Campo Magnético
 % Iniciar el cronómetro
 tic
 
@@ -28,7 +29,7 @@ fprintf('Campo Magnético en el punto (%f, %f, %f): %e T\n', punto_x, punto_y, p
 [Y, Z] = meshgrid(y, z);
 Y = flipud(Y);
 Z = flipud(Z);
-quiver(Y, Z, bcy./sqrt(bcy.^2+bcz.^2), bcz./sqrt(bcy.^2+bcz.^2));
+quiver(Y, Z, bcy ./ sqrt(bcy.^2 + bcz.^2), bcz ./ sqrt(bcy.^2 + bcz.^2));
 xlabel('y');
 ylabel('z');
 title('Campo Magnético');
@@ -39,40 +40,39 @@ tiempo_transcurrido = toc;
 % Mostrar el tiempo transcurrido en segundos
 fprintf('El tiempo de ejecución es: %.4f segundos.\n', tiempo_transcurrido);
 
+% Función para calcular el campo magnético
 function [bcx, bcy, bcz, y, z] = calcularCampoMagnetico(r, i, n)
-    
     % Generamos la matriz/malla
     y = linspace(-3*r, 3*r, 21);
     z = linspace(-r, 5*r, 21);
     [Y, Z] = meshgrid(y, z);
-    
+
     % Inicializamos los vectores para almacenar los componentes del campo magnético
     bcx = zeros(length(z), length(y));
     bcy = zeros(length(z), length(y));
     bcz = zeros(length(z), length(y));
-    
+
     % Bucle para calcular el campo magnético en cada punto de la malla
     for ind = 1:n
         % Calculamos el ángulo theta correspondiente al paso de integración
         theta = (ind - 1) * 2 * pi / n;
-        
+
         % Calculamos la distancia al punto de la malla y la elevamos a la tercera potencia
         d = sqrt(r^2 + Y.^2 + Z.^2 - (2*r)*Y*(sin(theta))).^3;
-        
+
         % Calculamos los componentes del campo magnético
         bcx = bcx + cos(theta) ./ d;
         bcy = bcy + sin(theta) ./ d;
         bcz = bcz + (r - Y .* sin(theta)) ./ d;
     end
-    
+
     % Calculamos el campo magnético completo
     bcx = 1E-7 * r * i * Z .* bcx * 2 * pi / n;
     bcy = 1E-7 * r * i * Z .* bcy * 2 * pi / n;
     bcz = 1E-7 * r * i * bcz * 2 * pi / n;
-    
+
     % Invertimos los vectores para que estén en el orden adecuado
     bcx = flipud(bcx);
     bcy = flipud(bcy);
     bcz = flipud(bcz);
 end
-
